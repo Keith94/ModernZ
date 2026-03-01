9637088990ceb993603817174d4500e86e96936e
Keith94 <dominantspace@gmail.com>
2026-03-01 11:04:23 -0700

    fix: don't hide UI when dragging seekbar
    possible fix

--- modernz.lua
+++ modernz.lua
@@ -3668,7 +3668,7 @@ local function render()
     if state.showtime ~= nil and get_hidetimeout() >= 0 then
         local timeout = state.showtime + (get_hidetimeout() / 1000) - now
         if timeout <= 0 and get_touchtimeout() <= 0 then
-            if state.active_element == nil and not mouse_over_osc or not user_opts.osc_keep_with_cursor then
+            if state.active_element == nil and not mouse_over_osc or not user_opts.osc_keep_with_cursor and not state.playing_and_seeking then
                 hide_osc()
             end
         else
